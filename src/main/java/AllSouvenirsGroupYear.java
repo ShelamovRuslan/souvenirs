59731a24f387036e7af8d5ca58ada2d504a71db7
Shelamov Ruslan <shelamov.ruslan@gmail.com>
2022-06-05 21:13:50 +0300

    56

--- src/main/java/AllSouvenirsGroupYear.java
+++ src/main/java/AllSouvenirsGroupYear.java
@@ -5,10 +5,18 @@ import java.util.Set;
 import java.util.stream.Collectors;
 
 public class AllSouvenirsGroupYear {
-    public void viewAllSouvenirsGroupYear() {
-        ArrayList<Souvenirs> list = new ArrayListSouvenirs().getArrayListSouvenirs();
-        Map<String, List<Souvenirs>> mapSouvenirs = list.stream().filter(s -> s.getPrice() != 0)
+
+    private ArrayList<Souvenirs> list;
+    private Map<String, List<Souvenirs>> mapSouvenirs;
+
+
+    public AllSouvenirsGroupYear() {
+        this.list = new ArrayListSouvenirs().getArrayListSouvenirs();
+        this.mapSouvenirs = list.stream().filter(s -> s.getPrice() != 0)
                 .collect(Collectors.groupingBy(Souvenirs::getProductionYear));
+    }
+
+    public void viewAllSouvenirsGroupYear() {
         Set<String> set = mapSouvenirs.keySet();
         set.forEach(s ->{
             System.out.printf("\nВ %s году были произведены:\n", mapSouvenirs.get(s).get(0).getProductionYear());
