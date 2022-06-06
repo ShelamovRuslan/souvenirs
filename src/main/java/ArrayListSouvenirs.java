f3df3e974368d5e313d9476ef0dcd7b547071354
Shelamov Ruslan <shelamov.ruslan@gmail.com>
2022-06-06 13:47:43 +0300

    62

--- src/main/java/ArrayListSouvenirs.java
+++ src/main/java/ArrayListSouvenirs.java
@@ -1,13 +1,16 @@
 import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.Set;
 
 public class ArrayListSouvenirs {
-
+    HashMap<String, ArrayList<Souvenirs>> map;
     private ArrayList<Souvenirs> list = new ArrayList<>();
 
     public ArrayListSouvenirs() {
-        CatalogSouvenirs catalogSouvenirsTemp = new CatalogSouvenirs();
-        catalogSouvenirsTemp.restore();
-        catalogSouvenirsTemp.getCatalogSouvenirs().forEach((s, souvenirs) -> this.list.add(souvenirs));
+        new NewCatalogSouvenirs().restoreCatalog();
+        this.map = NewCatalogSouvenirs.getCatalogSouvenirs();
+        Set<String> names = this.map.keySet();
+        names.forEach(name -> list.addAll(this.map.get(name)));
     }
 
     public ArrayList<Souvenirs> getArrayListSouvenirs() {
